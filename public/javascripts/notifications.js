62866321cf33a5414209b94b8665ea2e8ee8ed12
Ruth Ayers <ruth.ayers@outlook.com>
2015-02-28 23:35:13 +0000

    updated website 2

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,56 +1,35 @@
 // farmerlist data array for filling in info box
-var farmerListData = [];
+var textData = [];
 
 var farmerIDToUpdate = "";
 
 // DOM Ready =============================================================
 $(document).ready(function() {
 
-    // farmername link click
-    $('#farmerList table tbody').on('click', 'td a.linkshowfarmer', showfarmerInfo);
-
     // Add farmer button click
-    $('#btnAddfarmer').on('click', addfarmer);
-
-    // Delete farmer link click
-    $('#farmerList table tbody').on('click', 'td a.linkdeletefarmer', deletefarmer);
-
-    // Update farmer link click
-    $('#farmerList table tbody').on('click', 'td a.linkupdatefarmer', updatefarmerRetrieve);
-
-    // Update farmer update button
-     $('#btnUpdatefarmer').on('click', updatefarmerSubmit);
-
-    // Populate the farmer table on initial page load
-    populateTable();
+    $('#farmer1').on('click', loadFarmer1);
 
 });
 
 // Functions =============================================================
 
 // Fill table with data
-function populateTable() {
+function loadFarmer1() {
+  
 
-    // Empty content string
-    var tableContent = '';
+  document.getElementById('conversationContainer').innerHTML = 'New Header';
 
-    // jQuery AJAX call for JSON
-    $.getJSON( '/farmers/farmerlist', function( data ) {
-        
-        // Stick our farmer data array into a farmerlist variable in the global object
-        farmerListData = data;
+};
 
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowfarmer" rel="' + this.farmername + '">' + this.farmername + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeletefarmer" rel="' + this._id + '">delete</a></td>';
-            tableContent += '<td><a href="#" class="linkupdatefarmer" rel="' + this._id + '">update</a></td>';
-            tableContent += '</tr>';
-        });
+//create reply function that appends a div to the conversation window given a message
+function appendReply(message) {
+  $("#conversationContainer").prepend("<row><div id='reply'> Reply from farmer </div></row>");
+  //$("#conversationContainer").insertBefore( "<row><div id='reply'> Reply from farmer </div></row>" , $("#conversationContainer").firstChild);
+};
 
-        // Inject the whole content string into our existing HTML table
-        $('#farmerList table tbody').html(tableContent);
-    });
+//create reply function that appends a div to the conversation window given a message
+function appendQuestion(message) {
+  $("#conversationContainer").prepend("<row><div id='question'> Question to farmer </div></row>");
+      //$('#conversationContainer').animate({ scrollBottom: $(document).height()-$(window).height() }, 500);
+  //$("#conversationContainer").insertBefore( "<row><div id='reply'> Reply from farmer </div></row>" , $("#conversationContainer").firstChild);
 };
