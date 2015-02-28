80f00e7bc12fc6aca1c79e5aa8b43822726aa528
Ruth Ayers <ruth.ayers@outlook.com>
2015-02-28 17:24:48 +0000

    first website

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hi Ruth' });
 });
 
+/* GET notificatons page. */
+router.get('/notifications', function(req, res, next) {
+  res.render('notifications', { title: 'Hi Ruth' });
+});
+
+/* GET bloomberg page. */
+router.get('/bloomberg', function(req, res, next) {
+  res.render('bloomberg', { title: 'Hi Ruth' });
+});
+
 module.exports = router;
