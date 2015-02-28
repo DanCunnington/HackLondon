6577a579e0ce937e478cb222e4a43ae227224fc2
Daniel Cunnington <dcunnington1@sheffield.ac.uk>
2015-02-28 23:04:40 +0000

    messages now saving to database

--- routes/farmers.js
+++ routes/farmers.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
+//Twilio connect - using test credentials for now
+var client = require('twilio')('AC93f083af157194e9e51473461236bbe8','a177df398f82f481ec819a1c828f57cb');
+
 /* GET farmers page. */
 router.get('/', function(req, res, next) {
 
@@ -51,6 +55,58 @@ router.put('/updatefarmer/:id', function(req, res) {
     db.collection('farmers').updateById(farmerToUpdate, doc ,function(err, result) {
       res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
     });
+});
+
+/*
+* POST to message farmer - connect to Twilio
+*/
+router.post('/messageFarmer', function(req,res) {
+
+	//Extract the request
+	var request = req.body;
+		
+	var message = request.message;
+	var phoneNumber = request.farmer_phoneNumber;
+
+	console.log(phoneNumber);
+	console.log(message);
+
+	var messageObjectToInsert = {"farmer_id": request.farmer_id, "message_body": message, "replied": false };
+
+	//Save to database
+	var db = req.db;
+    db.collection('messages').insert(messageObjectToInsert, function(error, result){
+
+    	//Send an SMS text message
+		client.sendMessage({
+
+		    to: phoneNumber, // Any number Twilio can deliver to
+		    from: '+441384901173', // A number you bought from Twilio and can use for outbound communication
+		    body: message // body of the SMS message
+
+		}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+		    if (!err) { // "err" is an error received during the request, if any
+
+		        // "responseData" is a JavaScript object containing data received from Twilio.
+		        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+		        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+		        console.log(responseData.from); // outputs "+14506667788"
+		        console.log(responseData.body); // outputs "word to your mother."
+
+		        res.send(200);
+		    } else {
+		    	console.log(err);
+		    }
+		});
+        
+    });
+
+
+	
+
+
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -56,30 +56,6 @@ router.get('/bloomberg', function(req, res, next) {
 
 /* ---------------------------------------------------------------------------------*/
 
-//GET Request that sends the text message via twilio
-router.get('/sendTextMessage', function(req, res, next) {
-	
-	//Send an SMS text message
-	client.sendMessage({
-
-	    to:'+447961708658', // Any number Twilio can deliver to
-	    from: '+441384901173', // A number you bought from Twilio and can use for outbound communication
-	    body: 'Hey Farmer, please send us some data' // body of the SMS message
-
-	}, function(err, responseData) { //this function is executed when a response is received from Twilio
-
-	    if (!err) { // "err" is an error received during the request, if any
-
-	        // "responseData" is a JavaScript object containing data received from Twilio.
-	        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-	        console.log(responseData.from); // outputs "+14506667788"
-	        console.log(responseData.body); // outputs "word to your mother."
-	    }
-	});
-
-});
 
 
 // POST route for the text message reply
@@ -98,18 +74,6 @@ router.post('/textMessageReply', function(req,res) {
 
 	});
     res.send(200);
-    /*
-
-    res.render('index', { title: 'Message Sent' });
-
-	// Create a TwiML response
-    var resp = new client.TwimlResponse();
-	res.writeHead(200, {
-        'Content-Type':'text/xml'
-    });
-   
-    res.end(resp.toString());
-    */
 });
 
 /* GET notificatons page. */
