e8b0952d2d22516477e851833697d078597925fb
Eunjin Lee <eunjin.lee8@gmail.com>
2015-02-28 14:18:30 +0000

    google

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+
+	var out_message = "Hey please send us some data";
+	www.googleapis.com/language/translate/v2?key=AIzaSyBxO4Dar2Q_4zTurAGYfWOgeu4Ngewb4SE&q="+out_message+"&source=en&target=es;
+
+	$.get("www.googleapis.com/language/translate/v2?key=AIzaSyBxO4Dar2Q_4zTurAGYfWOgeu4Ngewb4SE&q="+out_message+"&source=en&target=es;", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+
   res.render('index', { title: 'Hi Ellie' });
 });
 
